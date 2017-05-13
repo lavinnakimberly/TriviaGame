53078bedb43f1831ab88f03e34e961629b954436
lavinnakimberly <lavinnakimberly@gmail.com>
2017-05-12 23:29:28 -0700

    files updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 			questionNumber++;
 			console.log("correct")
 			$("#question").empty();
-			$(".answerListContainer").empty();
+			$(".answersListContainer").empty();
 			displayQuestion(questionNumber);
 		}
 		else {
